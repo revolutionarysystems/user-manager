19b43975188eff59ab47d55e78e18366c62e6ac6
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-26 12:38:30 +0100

    Allowed FormAuthenticationFilter to return a 401 as well as redirecting based on config param

--- core/src/main/java/uk/co/revsys/user/manager/filter/FormAuthenticationFilter.java
+++ core/src/main/java/uk/co/revsys/user/manager/filter/FormAuthenticationFilter.java
@@ -1,8 +1,11 @@
 package uk.co.revsys.user.manager.filter;
 
+import java.io.IOException;
+import java.util.logging.Level;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
@@ -11,25 +14,48 @@ import org.slf4j.LoggerFactory;
 
 public class FormAuthenticationFilter extends AuthenticatingFilter {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(FormAuthenticationFilter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(FormAuthenticationFilter.class);
+    private boolean returnResponseCode = false;
 
-	@Override
-	protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
-		return new UsernamePasswordToken(request.getParameter("username"), request.getParameter("password"));
-	}
+    public boolean getReturnResponseCode() {
+        return returnResponseCode;
+    }
 
-	@Override
-	protected boolean isLoginRequest(ServletRequest request, ServletResponse response) {
-		return request.getParameter("username")!=null && request.getParameter("password")!=null;
-	}
+    public void setReturnResponseCode(boolean returnResponseCode) {
+        this.returnResponseCode = returnResponseCode;
+    }
 
-	@Override
-	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-		if (isLoginRequest(request, response)) {
-			return executeLogin(request, response);
-		} else {
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
-			return false;
-		}
-	}
+    @Override
+    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
+        return new UsernamePasswordToken(request.getParameter("username"), request.getParameter("password"));
+    }
+
+    @Override
+    protected boolean isLoginRequest(ServletRequest request, ServletResponse response) {
+        return request.getParameter("username") != null && request.getParameter("password") != null;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        if (isLoginRequest(request, response)) {
+            return executeLogin(request, response);
+        } else {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+    }
+
+    @Override
+    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
+        if (getReturnResponseCode()) {
+            try {
+                ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            } catch (IOException ex) {
+
+            }
+            return false;
+        } else {
+            return super.onLoginFailure(token, e, request, response);
+        }
+    }
 }
