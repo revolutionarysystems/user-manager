bfdc08ecb4cbc05979d13e364754d9fdfa16581d
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-26 13:00:05 +0100

    Added catches for login exceptions

--- core/src/main/java/uk/co/revsys/user/manager/servlet/LoginServlet.java
+++ core/src/main/java/uk/co/revsys/user/manager/servlet/LoginServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.subject.Subject;
@@ -17,7 +19,7 @@ public class LoginServlet extends HttpServlet {
 
     private String successUrl;
     private String loginUrl;
-    
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
@@ -25,23 +27,30 @@ public class LoginServlet extends HttpServlet {
         ShiroFilterFactoryBean shiroFactoryFactoryBean = webApplicationContext.getBean(ShiroFilterFactoryBean.class);
         this.successUrl = shiroFactoryFactoryBean.getSuccessUrl();
         this.loginUrl = shiroFactoryFactoryBean.getLoginUrl();
+        // Shiro uses urls that start with a / to be relative to the webapp, tomcat interprets this as relative to the application root directory (ie. webapps)
+        if(this.loginUrl.startsWith("/")){
+            this.loginUrl = this.loginUrl.substring(1);
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("LoginServlet.doGet");
         resp.sendRedirect(loginUrl);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("LoginServlet.doPost");
         Subject subject = SecurityUtils.getSubject();
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-        subject.login(new UsernamePasswordToken(username, password));
-        System.out.println("Login Success - redirecting to " + successUrl);
-        resp.sendRedirect(successUrl);
+        try {
+            subject.login(new UsernamePasswordToken(username, password));
+            resp.sendRedirect(successUrl); 
+        } catch (UnknownAccountException ex) {
+            resp.sendRedirect(loginUrl);
+        } catch(IncorrectCredentialsException ex){
+            resp.sendRedirect(loginUrl);
+        }
     }
 
 }
