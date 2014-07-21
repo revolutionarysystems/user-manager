70b77121cf5a2ca3671c19e036e72c089c140699
Adam Knight <adam.knight@opensquare.co.uk>
2014-07-21 09:14:57 +0100

    Added upload profile picture endpoint

--- service/src/main/java/uk/co/revsys/user/manager/service/rest/UserRestService.java
+++ service/src/main/java/uk/co/revsys/user/manager/service/rest/UserRestService.java
@@ -19,6 +19,8 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.apache.wink.common.model.multipart.BufferedInMultiPart;
+import org.apache.wink.common.model.multipart.InPart;
 import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import uk.co.revsys.user.manager.dao.exception.DAOException;
@@ -30,83 +32,83 @@ import uk.co.revsys.user.manager.service.Constants;
 import uk.co.revsys.user.manager.service.UserService;
 
 @Path("/users")
-public class UserRestService extends EntityRestService<User, UserService>{
+public class UserRestService extends EntityRestService<User, UserService> {
 
-	public UserRestService(UserService service) {
-		super(service);
-	}
-    
+    public UserRestService(UserService service) {
+        super(service);
+    }
+
     @GET
     @Path("/me")
-	@Produces("application/json")
-	public Response me(){
-		try {
-			Subject subject = SecurityUtils.getSubject();
-			if(!subject.isAuthenticated()){
-				return Response.status(Response.Status.UNAUTHORIZED).build();
-			}
-			String userId = (String) subject.getPrincipal();
-			User user = getService().findById(userId);
-			return Response.ok(toJSONString(user)).build();
-		} catch (DAOException ex) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-		} catch (JsonProcessingException ex) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-	
-	@GET
-	@Path("/{id}/roles")
-	public Response getRoles(@PathParam("id") String userId){
+    @Produces("application/json")
+    public Response me() {
+        try {
+            Subject subject = SecurityUtils.getSubject();
+            if (!subject.isAuthenticated()) {
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            }
+            String userId = (String) subject.getPrincipal();
+            User user = getService().findById(userId);
+            return Response.ok(toJSONString(user)).build();
+        } catch (DAOException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } catch (JsonProcessingException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GET
+    @Path("/{id}/roles")
+    public Response getRoles(@PathParam("id") String userId) {
         System.out.println("get roles");
-		try {
-			User user = getService().findById(userId);
-			if(user==null){
-				return Response.status(Response.Status.BAD_REQUEST).build();
-			}
-            if(!isAuthorisedToFindById(user)){
+        try {
+            User user = getService().findById(userId);
+            if (user == null) {
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+            if (!isAuthorisedToFindById(user)) {
                 return Response.status(Response.Status.FORBIDDEN).build();
             }
-			List<Role> roles = getService().getRoles(user);
-			return Response.ok(toJSONString(roles)).build();
-		} catch (DAOException ex) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-		} catch (JsonProcessingException ex) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-	
-	@GET
-	@Path("/{id}/permissions")
-	public Response getPermissions(@PathParam("id") String userId){
+            List<Role> roles = getService().getRoles(user);
+            return Response.ok(toJSONString(roles)).build();
+        } catch (DAOException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } catch (JsonProcessingException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GET
+    @Path("/{id}/permissions")
+    public Response getPermissions(@PathParam("id") String userId) {
         System.out.println("get permissions");
-		try {
-			User user = getService().findById(userId);
-			if(user==null){
-				return Response.status(Response.Status.BAD_REQUEST).build();
-			}
-            if(!isAuthorisedToFindById(user)){
+        try {
+            User user = getService().findById(userId);
+            if (user == null) {
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+            if (!isAuthorisedToFindById(user)) {
                 return Response.status(Response.Status.FORBIDDEN).build();
             }
-			List<Permission> permissions = getService().getPermissions(user);
-			return Response.ok(toJSONString(permissions)).build();
-		} catch (DAOException ex) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-		} catch (JsonProcessingException ex) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-    
+            List<Permission> permissions = getService().getPermissions(user);
+            return Response.ok(toJSONString(permissions)).build();
+        } catch (DAOException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } catch (JsonProcessingException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     @POST
     @Path("/{id}/changePassword")
-    public Response changePassword(@PathParam("id") String userId, @FormParam("password") String password){
+    public Response changePassword(@PathParam("id") String userId, @FormParam("password") String password) {
         System.out.println("change password for " + userId + " to " + password);
         try {
             User user = getService().findById(userId);
-            if(user==null){
+            if (user == null) {
                 return Response.status(Response.Status.BAD_REQUEST).build();
             }
-            if(!isUser(user)){
+            if (!isUser(user)) {
                 return Response.status(Response.Status.FORBIDDEN).build();
             }
             getService().changePassword(user, password);
@@ -119,33 +121,33 @@ public class UserRestService extends EntityRestService<User, UserService>{
             return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
         }
     }
-    
+
     @GET
     @Path("/{id}/profilePicture")
-    public Response getProfilePicture(@PathParam("id") String userId){
+    public Response getProfilePicture(@PathParam("id") String userId) {
         try {
             User user = getService().findById(userId);
-            if(user == null){
+            if (user == null) {
                 return Response.status(Response.Status.BAD_REQUEST).build();
             }
             InputStream profilePicture = getService().getProfilePicture(user);
             return Response.ok(profilePicture, MediaType.IMAGE_PNG_VALUE).build();
         } catch (DAOException ex) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        } catch(FileNotFoundException ex){
+        } catch (FileNotFoundException ex) {
             return Response.status(Response.Status.NOT_FOUND).build();
         } catch (IOException ex) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
-    
+
     @POST
     @Consumes(MediaType.IMAGE_PNG_VALUE)
     @Path("/{id}/profilePicture")
-    public Response setProfilePicture(@PathParam("id") String userId, InputStream pictureStream){
+    public Response setProfilePicture(@PathParam("id") String userId, InputStream pictureStream) {
         try {
             User user = getService().findById(userId);
-            if(user == null){
+            if (user == null) {
                 return Response.status(Response.Status.BAD_REQUEST).build();
             }
             String base64Picture = IOUtils.toString(pictureStream);
@@ -159,41 +161,63 @@ public class UserRestService extends EntityRestService<User, UserService>{
         }
     }
 
-	@Override
-	protected boolean isAuthorisedToDelete(User e) {
-		return isAdministrator() || (isAccountOwner(e) && !e.getRoles().contains(Constants.ACCOUNT_OWNER_ROLE));
-	}
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Path("/{id}/profilePicture")
+    public Response uploadProfilePicture(@PathParam("id") String userId, BufferedInMultiPart bufferedInMultiPart) {
+        try {
+            User user = getService().findById(userId);
+            if (user == null) {
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+            List<InPart> parts = bufferedInMultiPart.getParts();
+            InPart part = parts.get(0);
+            String contentDispositionHeaderValue = part.getHeaders().getFirst("Content-Disposition");
+            InputStream pictureStream = part.getInputStream();
+            getService().setProfilePicture(user, pictureStream);
+            return Response.status(Response.Status.OK).build();
+        } catch (DAOException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } catch (IOException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
-	@Override
-	protected boolean isAuthorisedToUpdate(User e) {
-		return isAdministrator() || isAccountOwner(e) || isUser(e);
-	}
+    @Override
+    protected boolean isAuthorisedToDelete(User e) {
+        return isAdministrator() || (isAccountOwner(e) && !e.getRoles().contains(Constants.ACCOUNT_OWNER_ROLE));
+    }
 
-	@Override
-	protected boolean isAuthorisedToFindById(User e) {
-		return isAdministrator() || isAccountOwner(e) || isUser(e);
-	}
+    @Override
+    protected boolean isAuthorisedToUpdate(User e) {
+        return isAdministrator() || isAccountOwner(e) || isUser(e);
+    }
 
-	@Override
-	protected boolean isAuthorisedToCreate(User e) {
-		return isAdministrator() || isAccountOwner(e);
-	}
-	
-	protected boolean isAccountOwner(User e){
-		Subject subject = SecurityUtils.getSubject();
-		User user = subject.getPrincipals().oneByType(User.class);
-		return subject.hasRole(Constants.ACCOUNT_OWNER_ROLE) && user.getAccount().equals(e.getAccount());
-	}
-	
-	protected boolean isUser(User e){
-		Subject subject = SecurityUtils.getSubject();
-		User user = subject.getPrincipals().oneByType(User.class);
-		return user.getId().equals(e.getId());
-	}
+    @Override
+    protected boolean isAuthorisedToFindById(User e) {
+        return isAdministrator() || isAccountOwner(e) || isUser(e);
+    }
+
+    @Override
+    protected boolean isAuthorisedToCreate(User e) {
+        return isAdministrator() || isAccountOwner(e);
+    }
+
+    protected boolean isAccountOwner(User e) {
+        Subject subject = SecurityUtils.getSubject();
+        User user = subject.getPrincipals().oneByType(User.class);
+        return subject.hasRole(Constants.ACCOUNT_OWNER_ROLE) && user.getAccount().equals(e.getAccount());
+    }
+
+    protected boolean isUser(User e) {
+        Subject subject = SecurityUtils.getSubject();
+        User user = subject.getPrincipals().oneByType(User.class);
+        return user.getId().equals(e.getId());
+    }
 
     @Override
     protected JSONObject filter(JSONObject json, User user) {
-        if(!(isAdministrator() || isAccountOwner(user))){
+        if (!(isAdministrator() || isAccountOwner(user))) {
             json.remove("password");
             json.remove("passwordSalt");
             json.remove("account");
@@ -204,9 +228,9 @@ public class UserRestService extends EntityRestService<User, UserService>{
         return json;
     }
 
-	@Override
-	protected Class<? extends User> getEntityType() {
-		return User.class;
-	}
+    @Override
+    protected Class<? extends User> getEntityType() {
+        return User.class;
+    }
 
 }
