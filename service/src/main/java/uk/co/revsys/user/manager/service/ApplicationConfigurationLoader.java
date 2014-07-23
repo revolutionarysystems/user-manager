2ee73cd225a3a5faab068af3f72661e98327a403
Adam Knight <adam.knight@opensquare.co.uk>
2014-07-23 11:05:33 +0100

    Removed default user and account

--- service/src/main/java/uk/co/revsys/user/manager/service/ApplicationConfigurationLoader.java
+++ service/src/main/java/uk/co/revsys/user/manager/service/ApplicationConfigurationLoader.java
@@ -3,10 +3,6 @@ package uk.co.revsys.user.manager.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
 import javax.validation.ConstraintViolationException;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
@@ -16,21 +12,28 @@ import org.slf4j.LoggerFactory;
 import uk.co.revsys.resource.repository.provider.handler.FilteringResourceHandler;
 import uk.co.revsys.user.manager.dao.exception.DAOException;
 import uk.co.revsys.user.manager.dao.exception.DuplicateKeyException;
+import uk.co.revsys.user.manager.model.AbstractEntity;
+import uk.co.revsys.user.manager.model.Account;
 import uk.co.revsys.user.manager.model.Application;
 import uk.co.revsys.user.manager.model.Permission;
 import uk.co.revsys.user.manager.model.Role;
+import uk.co.revsys.user.manager.model.User;
 
 public class ApplicationConfigurationLoader implements FilteringResourceHandler {
 
     private final Logger LOGGER = LoggerFactory.getLogger(ApplicationConfigurationLoader.class);
-    
+
+    private final AccountService accountService;
+    private final UserService userService;
     private final EntityService<Application> applicationService;
     private final RoleService roleService;
     private final EntityService<Permission> permissionService;
-    
+
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public ApplicationConfigurationLoader(EntityService<Application> applicationService, RoleService roleService, EntityService<Permission> permissionService) {
+    public ApplicationConfigurationLoader(AccountService accountService, UserService userService, EntityService<Application> applicationService, RoleService roleService, EntityService<Permission> permissionService) {
+        this.accountService = accountService;
+        this.userService = userService;
         this.applicationService = applicationService;
         this.roleService = roleService;
         this.permissionService = permissionService;
@@ -41,14 +44,18 @@ public class ApplicationConfigurationLoader implements FilteringResourceHandler 
         try {
             String jsonString = IOUtils.toString(contents);
             JSONObject json = new JSONObject(jsonString);
-            createPermissions(json.getJSONArray("permissions"));
-            createRoles(json.getJSONArray("roles"));
-            createApplication(json);
+            createEntities(permissionService, Permission.class, json.optJSONArray("permissions"));
+            createEntities(roleService, Role.class, json.optJSONArray("roles"));
+            createEntities(applicationService, Application.class, json.optJSONArray("applications"));
+            createEntities(accountService, Account.class, json.optJSONArray("accounts"));
+            createEntities(userService, User.class, json.optJSONArray("users"));
         } catch (DAOException ex) {
             LOGGER.error("Unable to update application from configuration: " + resource.getName(), ex);
         } catch (DuplicateKeyException ex) {
             LOGGER.error("Unable to update application from configuration: " + resource.getName(), ex);
-        } catch(ConstraintViolationException ex){
+        } catch (ConstraintViolationException ex) {
+            LOGGER.error("Unable to update application from configuration: " + resource.getName(), ex);
+        } catch (IOException ex) {
             LOGGER.error("Unable to update application from configuration: " + resource.getName(), ex);
         }
     }
@@ -58,80 +65,17 @@ public class ApplicationConfigurationLoader implements FilteringResourceHandler 
         return resource.getName().endsWith(".json");
     }
 
-    private void createApplication(JSONObject applicationJson) throws DAOException, IOException, DuplicateKeyException {
-        String id = applicationJson.getString("id");
-        Application application = new Application();
-        application.setId(id);
-        application.setName(applicationJson.getString("name"));
-        application.setDescription(applicationJson.getString("description"));
-        List<String> permissions = new ArrayList<String>();
-        JSONArray permissionsJson = applicationJson.getJSONArray("permissions");
-        for (int i = 0; i < permissionsJson.length(); i++) {
-            permissions.add(permissionsJson.getJSONObject(i).getString("id"));
-        }
-        application.setPermissions(permissions);
-        List<String> roles = new ArrayList<String>();
-        JSONArray rolesJson = applicationJson.getJSONArray("roles");
-        for (int i = 0; i < rolesJson.length(); i++) {
-            roles.add(rolesJson.getJSONObject(i).getString("id"));
-        }
-        application.setRoles(roles);
-        JSONObject attributesJson = applicationJson.getJSONObject("attributes");
-        Map attributes = objectMapper.readValue(attributesJson.toString(), Map.class);
-        application.setAttributes(attributes);
-        if (applicationService.findById(id) == null) {
-            try {
-                applicationService.create(application);
-            } catch (DuplicateKeyException ex) {
-                // Ignore as impossible
-            }
-        } else {
-            applicationService.update(application);
-        }
-    }
-
-    private void createRoles(JSONArray roles) throws DAOException, DuplicateKeyException {
-        for (int i = 0; i < roles.length(); i++) {
-            JSONObject roleJson = roles.getJSONObject(i);
-            String id = roleJson.getString("id");
-            Role role = new Role();
-            role.setId(id);
-            role.setName(roleJson.getString("name"));
-            role.setDescription(roleJson.getString("description"));
-            List<String> permissions = new ArrayList<String>();
-            JSONArray permissionsJson = roleJson.getJSONArray("permissions");
-            for (int j = 0; j < permissionsJson.length(); j++) {
-                permissions.add(permissionsJson.getString(j));
-            }
-            role.setPermissions(permissions);
-            if (roleService.findById(id) == null) {
-                try {
-                    roleService.create(role);
-                } catch (DuplicateKeyException ex) {
-                    // Ignore as impossible
+    private void createEntities(EntityService service, Class<? extends AbstractEntity> entityType, JSONArray json) throws DAOException, DuplicateKeyException, IOException {
+        if (json != null) {
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject entityJson = json.getJSONObject(i);
+                String entityId = entityJson.optString("id");
+                AbstractEntity entity = objectMapper.readValue(entityJson.toString(), entityType);
+                if (entityId != null && service.findById(entityId) != null) {
+                    service.update(entity);
+                } else {
+                    service.create(entity);
                 }
-            } else {
-                roleService.update(role);
-            }
-        }
-    }
-
-    private void createPermissions(JSONArray permissions) throws DAOException, DuplicateKeyException {
-        for (int i = 0; i < permissions.length(); i++) {
-            JSONObject permissionJson = permissions.getJSONObject(i);
-            String id = permissionJson.getString("id");
-            Permission permission = new Permission();
-            permission.setId(id);
-            permission.setName(permissionJson.getString("name"));
-            permission.setDescription(permissionJson.getString("description"));
-            if (permissionService.findById(id) == null) {
-                try {
-                    permissionService.create(permission);
-                } catch (DuplicateKeyException ex) {
-                    // Ignore as impossible
-                }
-            } else {
-                permissionService.update(permission);
             }
         }
     }
--- service/src/main/java/uk/co/revsys/user/manager/service/ServiceInitializer.java
+++ service/src/main/java/uk/co/revsys/user/manager/service/ServiceInitializer.java
@@ -1,7 +1,5 @@
 package uk.co.revsys.user.manager.service;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import org.slf4j.Logger;
@@ -9,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import uk.co.revsys.user.manager.dao.exception.DAOException;
 import uk.co.revsys.user.manager.dao.exception.DuplicateKeyException;
-import uk.co.revsys.user.manager.model.Account;
 import uk.co.revsys.user.manager.model.Role;
-import uk.co.revsys.user.manager.model.Status;
-import uk.co.revsys.user.manager.model.User;
 
 public class ServiceInitializer implements ServletContextListener{
 	
@@ -22,47 +17,20 @@ public class ServiceInitializer implements ServletContextListener{
 	public void contextInitialized(ServletContextEvent sce) {
 		try {
 			ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-			EntityService<Account> accountService = (EntityService<Account>) applicationContext.getBean("accountService");
-			UserService userService = (UserService) applicationContext.getBean("userService");
 			RoleService roleService = (RoleService) applicationContext.getBean("roleService");
-			String masterAccountId = Constants.MASTER_ACCOUNT_ID;
-			String masterUserRoleId = Constants.ADMINSTRATOR_ROLE_ID;
-			String masterUserId = Constants.MASTER_USER_ID;
-			String accountOwnerRoleId = Constants.ACCOUNT_OWNER_ROLE_ID;
-			if(accountService.findById(masterAccountId)==null){
-				Account account = new Account();
-				account.setId(masterAccountId);
-				account.setName("Master Account");
-				account.setStatus(Status.enabled);
-				accountService.create(account);
-			}
-			if(roleService.findById(masterUserRoleId)==null){
+            if(roleService.findById(Constants.ADMINISTRATOR_ROLE)==null){
 				Role role = new Role();
-				role.setId(masterUserRoleId);
-				role.setName(masterUserRoleId);
+				role.setId(Constants.ADMINISTRATOR_ROLE);
+				role.setName(Constants.ADMINISTRATOR_ROLE);
 				role.setDescription("User Manager Administrator");
 				roleService.create(role);
 			}
-			if(roleService.findById(accountOwnerRoleId)==null){
+			if(roleService.findById(Constants.ACCOUNT_OWNER_ROLE)==null){
 				Role role = new Role();
-				role.setId(accountOwnerRoleId);
-				role.setName(accountOwnerRoleId);
+				role.setId(Constants.ACCOUNT_OWNER_ROLE);
+				role.setName(Constants.ACCOUNT_OWNER_ROLE);
 				role.setDescription("User Manager Account Owner");
 				roleService.create(role);
-			}
-			if(userService.findById(masterUserId) == null){
-				User user = new User();
-                user.setId(masterUserId);
-				user.setAccount(masterAccountId);
-				user.setName("Master User");
-				user.setUsername(masterUserId);
-				user.setPassword("changeme123");
-				user.setStatus(Status.enabled);
-				List<String> roles = new ArrayList<String>();
-				roles.add(masterUserRoleId);
-                roles.add(accountOwnerRoleId);
-				user.setRoles(roles);
-				userService.create(user);
 			}
 		} catch (DAOException ex) {
 			LOGGER.error("Unable to setup default data", ex);
