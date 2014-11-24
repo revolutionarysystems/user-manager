ce081be1aa8e6906c2b030ae0c73e69954acdd00
Adam Knight <adam@revolutionarysystems.co.uk>
2014-11-24 15:23:13 +0000

    Create session on login request if it doesn't exist

--- core/src/main/java/uk/co/revsys/user/manager/servlet/LoginServlet.java
+++ core/src/main/java/uk/co/revsys/user/manager/servlet/LoginServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
@@ -15,6 +16,7 @@ public abstract class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
         Subject subject = SecurityUtils.getSubject();
         String username = req.getParameter("username");
         String password = req.getParameter("password");
